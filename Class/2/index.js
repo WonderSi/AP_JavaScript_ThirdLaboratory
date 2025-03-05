1a66678a27e243ce81580d0f8e187e5f3a467934
WonderSi <kirillkozlov00@gmail.com>
2025-03-05 14:52:05 +0700

    create task class 2

---
+++ Class/2/index.js
@@ -0,0 +1,16 @@
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+}
+class Rabbit extends Animal {
+    constructor(name) {
+        super(name); // ###
+        this.created = Date.now();
+    }
+}
+let rabbit = new Rabbit("Белый кролик");
+console.log(rabbit.name);
+
+// Необходимо вызвать super() для вызова конструктора родительского класса (Animal)
+// Это нужно для корректной инициализации свойств, унаследованных от родителя
