8ee9511b51360918edc5830025811397367a0310
WonderSi <139754550+WonderSi@users.noreply.github.com>
2025-03-06 14:02:06 +0700

    Update index.js

--- Prototype/1/index.js
+++ Prototype/1/index.js
@@ -7,6 +7,6 @@ let rabbit = {
 };
 console.log(rabbit.jumps); // true (1)
 delete rabbit.jumps;
-console.log(rabbit.jumps); // false (2)
+console.log(rabbit.jumps); // null (2)
 delete animal.jumps;
 console.log(rabbit.jumps); // undefined (3)
